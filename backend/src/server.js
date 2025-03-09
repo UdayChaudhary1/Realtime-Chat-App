65821001d5f68bf116f61539f2fa711b758399f8
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-03-09 14:30:53 +0530

    Add endpoint for messageRoutes

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes)
 
 app.listen(3000, () => {
   console.log("App listening on port 3000!");
