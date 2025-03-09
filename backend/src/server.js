9bdad8b7ff2a941edbf6c7707180450bfdc83812
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-03-09 12:36:59 +0530

    Connect to database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
-import authRoutes from "./routes/auth.js";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+import "dotenv/config";
 
 const app = express();
 
@@ -7,4 +9,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   console.log("App listening on port 3000!");
+  connectDB();
 });
