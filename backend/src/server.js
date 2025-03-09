6acd1ef266174d3dded59c5a1abe8e971c617029
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-03-09 18:32:04 +0530

    Configure CORS for frontend integration

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,16 @@ import { connectDB } from "./lib/db.js";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes)
+app.use("/api/message", messageRoutes);
 
 app.listen(3000, () => {
   console.log("App listening on port 3000!");
