feb86bbeb9602503e5c135812bf9a15107b2a1a1
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-03-09 15:18:37 +0530

    Add routes to all the pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
 import Navbar from "./components/Navbar.jsx";
+import { Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SettingsPage from "./pages/SettingsPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import { Route } from "react-router-dom";
 
 const App = () => {
-  return <div>
-    <Navbar />
-  </div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 };
 export default App;
