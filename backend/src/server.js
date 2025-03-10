ee3b8eb8605d3483936f3dd54759d45fc02e11a4
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-03-10 10:40:31 +0530

    Set file upload size limit to 1MB

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,9 +8,14 @@ import cors from "cors";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "1mb" }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
