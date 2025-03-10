b62d6f995d44dae20d1fef70b72d6fc1a01f132e
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-03-10 09:05:16 +0530

    Correct the API endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(3000, () => {
   console.log("App listening on port 3000!");
